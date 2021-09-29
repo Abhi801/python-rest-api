1835d212fa002ba531e240e07cf9dcdb712a8c0b
Abhi801 <abhimandloi801@gmail.com>
2021-09-29 11:16:38 +0530

    Added viewset methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
@@ -48,3 +49,51 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """ Handling delete method"""
         return Response({'method':'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """ test API view set"""
+
+    serializer_class=serializers.HelloSerializer
+
+    def list(self, request):
+        """ Return a hello message with viewset list method"""
+
+        an_viewset=[
+        'Users actions (list,create,retrive,update,partial_update, destroy)',
+        'Automatically maps Urls using routers',
+        'Provide more fuctionality with lesss code'
+        ]
+
+        return Response({'message':'Hello','an_viewset':an_viewset})
+
+    def create(self, request):
+        """ Create an new hello message"""
+
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'hello,{name}'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def retrive(self, request, pk=None):
+        """ Creating new retrive method"""
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """ Creating new update method"""
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ Creating new partial_update method"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """ Creating new destroy method"""
+        return Response({'http_method':'Delete'})
